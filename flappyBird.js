b748d7a4ec8a5b522646ee00b97f830dcd9aeea5
vcordero07 <vcordero07@gmail.com>
2019-03-04 17:59:22 -0500

    wip-fully working flappy

--- flappyBird.js
+++ flappyBird.js
@@ -1,44 +1,98 @@
-  var cvs = document.getElementById("canvas");
-  var ctx = cvs.getContext("2d");
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
 
 
-  // load images
+// load images
 
-  var bird = new Image();
-  var bg = new Image();
-  var fg = new Image();
-  var pipeNorth = new Image();
-  var pipeSouth = new Image();
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeNorth = new Image();
+var pipeSouth = new Image();
 
-  bird.src = "images/bird.png";
-  bg.src = "images/bg.png";
-  fg.src = "images/fg.png";
-  pipeNorth.src = "images/pipeNorth.png";
-  pipeSouth.src = "images/pipeSouth.png";
+bird.src = "images/bird.png";
+bg.src = "images/bg.png";
+fg.src = "images/fg.png";
+pipeNorth.src = "images/pipeNorth.png";
+pipeSouth.src = "images/pipeSouth.png";
 
-  //variables
-  var gap = 85;
-  var constant = pipeNorth.height+gap;
-  var bX = 10;
-  var bY = 150;
-  var gravity = 1;
+//variables
+var gap = 85;
+var constant = pipeNorth.height+gap;
+var bX = 10;
+var bY = 150;
+var gravity = 1.5;
+var score = 0;
+
+//audio files
+var sndFly = new Audio();
+var sndScore = new Audio();
+
+sndFly.src = "sounds/fly.mp3";
+sndScore.src = "sounds/score.mp3";
 
 
+//on key down
+document.addEventListener("keydown", moveUp);
 
-  //draw images
-  function draw(){
-    ctx.drawImage(bg,0,0);
+function moveUp(){
+  bY -= 25;
+  sndFly.play();
+}
 
-    ctx.drawImage(pipeNorth,100,0);
-    ctx.drawImage(pipeSouth,100,0+constant);
+//pipe coordinates
+var pipe = [];
 
-    ctx.drawImage(fg, 0, cvs.height - fg.height);
+pipe[0] = {
+   x : cvs.width,
+   y : 0
+}
 
-    ctx.drawImage(bird, bX, bY);
+//draw images
+function draw(){
+  ctx.drawImage(bg,0,0);
 
-    bY += gravity;
+  for(var i=0; i<pipe.length; i++) {
 
-    requestAnimationFrame(draw);
+    ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+    ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y+constant);
+
+    pipe[i].x--;
+
+    if (pipe[i].x == 125) {
+      pipe.push({
+        x : cvs.width,
+        y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
+      })
+    }
+
+    //detect collision
+    if (bX + bird.width >= pipe[i].x
+      && bX <= pipe[i].x + pipeNorth.width
+      && (bY <= pipe[i].y + pipeNorth.height
+      || bY+bird.height >= pipe[i].y+constant)
+      || bY + bird.height >= cvs.height - fg.height) {
+      location.reload(); //reload the page
+    }
+
+    if (pipe[i].x == 5) {
+      score++;
+      sndScore.play();
+    }
   }
 
-  draw();
+
+  ctx.drawImage(fg, 0, cvs.height - fg.height);
+
+  ctx.drawImage(bird, bX, bY);
+
+  bY += gravity;
+
+  ctx.fillStyle = "#000";
+  ctx.font = "20px ZCOOL QingKe HuangYou";
+  ctx.fillText("Score : " + score, 10, cvs.height-20);
+
+  requestAnimationFrame(draw);
+}
+
+draw();
